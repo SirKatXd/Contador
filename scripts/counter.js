e8f9561b2efe516166ef61f94d9e4361f8d079b2
Shion <jorgej.jauregui99@gmail.com>
2023-07-05 15:48:08 -0500

    Correcciones menores

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,8 +1,22 @@
+// Contador //
 let contador = 0;
+let maxIntentos = 0;
 
+
+
+//Funciones de control del contador//
 function incrementarContador() {
   contador++
   document.getElementById("contador").innerHTML = contador;
+  var intentosInput = document.getElementById('intentosFb');
+      maxIntentos = parseInt(intentosInput.value);
+
+      if (contador > maxIntentos) {
+        alert('Se alcanzó la cantidad máxima de intentos.');
+        contador = maxIntentos;
+      }
+
+      document.getElementById('contador').textContent = contador;
 }
 
 function reducirContador() {
@@ -21,11 +35,23 @@ function reducirContador() {
 function reiniciarContador() {
   contador = 0
   document.getElementById("contador").innerHTML = contador;
+  document.getElementById('contador').textContent = contador;
 
 
 }
+
+//Funciones de control para los intentos
 
 function borrarIntentos() {
   var intentosInput = document.querySelector('input[name="intentosFb"]');
   intentosInput.value = '';
 }
+
+//Identificador de intentos//
+
+function obtenerValorIntentos() {
+  var intentosInput = document.getElementById('intentosFb');
+  var valorIntentos = intentosInput.value;
+  console.log('El valor del campo de intentos es: ' + valorIntentos);
+}
+
