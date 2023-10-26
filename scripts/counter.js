ee61b9231532a8311223fb36bd06320915b8e507
SirKat Xd <jorgej.jauregui99@gmail.com>
2023-10-26 16:30:41 -0500

    Fixed counter attemps

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,56 +2,48 @@
 let contador = 0;
 let maxIntentos = 0;
 
-
-
 //Funciones de control del contador//
 function incrementarContador() {
-  contador++
+  contador++;
   document.getElementById("contador").innerHTML = contador;
-  var intentosInput = document.getElementById('intentosFb');
-      maxIntentos = parseInt(intentosInput.value);
+  var intentosInput = document.getElementById("intentosFb");
+  maxIntentos = parseInt(intentosInput.value);
 
-      if (contador > maxIntentos) {
-        alert('Se alcanzó la cantidad máxima de intentos.');
-        contador = maxIntentos;
-      }
+  if (contador > maxIntentos) {
+    alert("Se alcanzó la cantidad máxima de intentos.");
+    contador = maxIntentos;
+  }
 
-      document.getElementById('contador').textContent = contador;
+  document.getElementById("contador").textContent = contador;
 }
 
 function reducirContador() {
-  contador--
+  contador--;
   document.getElementById("contador").innerHTML = contador;
 
-  if(contador<0){
-    
-    alert("Los numeros negativos no están permitidos >:v ")
-    reiniciarContador()
-  
+  if (contador < 0) {
+    alert("Los numeros negativos no están permitidos >:v ");
+    reiniciarContador();
   }
 }
 
-
 function reiniciarContador() {
-  contador = 0
+  contador = 0;
   document.getElementById("contador").innerHTML = contador;
-  document.getElementById('contador').textContent = contador;
-
-
+  document.getElementById("contador").textContent = contador;
 }
 
 //Funciones de control para los intentos
 
 function borrarIntentos() {
   var intentosInput = document.querySelector('input[name="intentosFb"]');
-  intentosInput.value = '';
+  intentosInput.value = "";
 }
 
 //Identificador de intentos//
 
 function obtenerValorIntentos() {
-  var intentosInput = document.getElementById('intentosFb');
+  var intentosInput = document.getElementById("intentosFb");
   var valorIntentos = intentosInput.value;
-  console.log('El valor del campo de intentos es: ' + valorIntentos);
+  console.log("El valor del campo de intentos es: " + valorIntentos);
 }
-
